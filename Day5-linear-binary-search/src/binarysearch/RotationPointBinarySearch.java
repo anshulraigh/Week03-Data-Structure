c865737779cf3914cefaa976265faac450749c94
Anshul Rai <anshulrai02092003@gmail.com>
2025-02-04 16:01:59 +0530

    [Anshul] Add . Find the Rotation Point in a Rotated Sorted Array

---
+++ Day5-linear-binary-search/src/binarysearch/RotationPointBinarySearch.java
@@ -0,0 +1,25 @@
+package binarysearch;
+
+public class RotationPointBinarySearch {
+    public static int findRotationPoint(int arr[]){
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] > arr[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 4, 5, 6, 7, 0, 1, 2 }; // Example rotated sorted array
+        int index = findRotationPoint(arr);
+        System.out.println("Index of the smallest element: " + index);
+        System.out.println("Smallest element: " + arr[index]);
+    }
+}
